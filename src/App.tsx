a76b35debcbdf179f9df86164841bd3c7ae6206c
lizunlong <lizunlong@gmail.com>
2019-12-09 11:01:12 +0800

    generate room id locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import nanoid from 'nanoid';
 
 const App: React.FC = () => {
+  const [roomId, setRoomId] = useState("")
+
+  useEffect(() => {
+    const roomId = localStorage.getItem('roomId')
+    if (roomId) setRoomId(roomId)
+  }, [roomId])
+
+  function handleNewRoomId() {
+    const roomId = nanoid()
+    console.log("Got a roomId:", roomId)
+    localStorage.setItem("roomId", roomId)
+    setRoomId(roomId)
+  }
+  
+  function handleJoinRoom() {
+    const roomId = localStorage.getItem('roomId')
+    console.log("Join room:", roomId)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {roomId ? <button onClick={handleJoinRoom}>Join Room: {roomId}</button> : <button onClick={handleNewRoomId}>Generate Room ID</button>}
     </div>
-  );
+  )
 }
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
