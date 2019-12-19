eb212d9761da8e33dec5f9e51eb5c134ed274848
lizunlong <lizunlong@gmail.com>
2019-12-19 09:48:09 +0800

    more data

--- api/open-data.ts
+++ api/open-data.ts
@@ -10,39 +10,55 @@ export default async (req: NowRequest, res: NowResponse) => {
     const data = await client.query(
       q.Let(
         {
-          date: "2019-12-18",
-          roomId: "251514846315545088"
+          date: q.ToString(q.ToDate(q.Now()))
+          // roomId: "251514846315545088"
         },
         {
           data: {
             room_creation_all_count: q.Count(
               q.Match(q.Index("logs_by_type_to_count"), ["room_creation"])
             ),
-            room_creation_day_count: q.Count(
+            room_joined_all_count: q.Count(
+              q.Match(q.Index("logs_by_type_to_count"), ["room_joined"])
+            ),
+            room_left_all_count: q.Count(
+              q.Match(q.Index("logs_by_type_to_count"), ["room_duration"])
+            ),
+            room_duration_all_sum: q.Sum(
+              q.Select(
+                ["data"],
+                q.Paginate(
+                  q.Match(q.Index("logs_by_type_return_duration"), [
+                    "room_duration"
+                  ])
+                )
+              )
+            ),
+            room_creation_today_count: q.Count(
               q.Match(q.Index("logs_by_type_and_createdDateString_to_count"), [
                 "room_creation",
                 q.Var("date")
               ])
             ),
-            room_joined_day_count: q.Count(
+            room_joined_today_count: q.Count(
               q.Match(q.Index("logs_by_type_and_createdDateString_to_count"), [
                 "room_joined",
                 q.Var("date")
               ])
             ),
-            room_left_day_count: q.Count(
+            room_left_today_count: q.Count(
               q.Match(q.Index("logs_by_type_and_createdDateString_to_count"), [
                 "room_duration",
                 q.Var("date")
               ])
             ),
-            room_joined_roomId_count: q.Count(
-              q.Match(q.Index("logs_by_type_and_roomId_to_count"), [
-                "room_joined",
-                q.Var("roomId")
-              ])
-            ),
-            room_duration_day_sum: q.Sum(
+            // room_joined_roomId_count: q.Count(
+            //   q.Match(q.Index("logs_by_type_and_roomId_to_count"), [
+            //     "room_joined",
+            //     q.Var("roomId")
+            //   ])
+            // ),
+            room_duration_today_sum: q.Sum(
               q.Select(
                 ["data"],
                 q.Paginate(
@@ -54,18 +70,18 @@ export default async (req: NowRequest, res: NowResponse) => {
                   )
                 )
               )
-            ),
-            room_duration_roomId_sum: q.Sum(
-              q.Select(
-                ["data"],
-                q.Paginate(
-                  q.Match(q.Index("logs_by_type_and_roomId_return_duration"), [
-                    "room_duration",
-                    q.Var("roomId")
-                  ])
-                )
-              )
             )
+            // room_duration_roomId_sum: q.Sum(
+            //   q.Select(
+            //     ["data"],
+            //     q.Paginate(
+            //       q.Match(q.Index("logs_by_type_and_roomId_return_duration"), [
+            //         "room_duration",
+            //         q.Var("roomId")
+            //       ])
+            //     )
+            //   )
+            // )
           }
         }
       )
