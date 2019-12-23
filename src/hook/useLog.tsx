5ffa59cb86238836bc17766f4970933a7060a40f
lizunlong <lizunlong@gmail.com>
2019-12-23 10:20:21 +0800

    send logs only if in production mode

--- src/hook/useLog.tsx
+++ src/hook/useLog.tsx
@@ -1,103 +1,118 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback } from 'react'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 function formatBody(body: any) {
-  const userName = localStorage.getItem("userName");
+  const userName = localStorage.getItem('userName')
   if (userName) {
-    body = { ...body, userName };
+    body = { ...body, userName }
   }
 
-  const deviceId = localStorage.getItem("deviceId");
+  const deviceId = localStorage.getItem('deviceId')
   if (deviceId) {
-    body = { ...body, deviceId };
+    body = { ...body, deviceId }
   }
 
-  return JSON.stringify(body);
+  return JSON.stringify(body)
 }
 
 export function useLogRoomVisited({ roomId }: any) {
   useEffect(() => {
-    if (!navigator.sendBeacon) return;
+    if (!isProduction) return
+    if (!navigator.sendBeacon) return
 
     const body = formatBody({
-      type: "room_visited",
-      roomId
-    });
-    navigator.sendBeacon("/api/log", body);
-  }, [roomId]);
+      type: 'room_visited',
+      roomId,
+    })
+    navigator.sendBeacon('/api/log', body)
+  }, [roomId])
 }
 
 export function useLogRoomJoined({ roomId }: any) {
   useEffect(() => {
-    if (!navigator.sendBeacon) return;
+    if (!isProduction) return
+    if (!navigator.sendBeacon) return
 
     const body = formatBody({
-      type: "room_joined",
-      roomId
-    });
-    navigator.sendBeacon("/api/log", body);
-  }, [roomId]);
+      type: 'room_joined',
+      roomId,
+    })
+    navigator.sendBeacon('/api/log', body)
+  }, [roomId])
 }
 
 export function useLogRoomInterval({ roomId }: any) {
   useEffect(() => {
-    if (!navigator.sendBeacon) return;
+    if (!isProduction) return
+    if (!navigator.sendBeacon) return
 
-    const duration = 5 * 60 * 1000; // every 5 minites
+    const duration = 5 * 60 * 1000 // every 5 minites
     const id = setInterval(() => {
       const body = formatBody({
-        type: "room_interval",
+        type: 'room_interval',
         roomId,
-        duration
-      });
-      navigator.sendBeacon("/api/log", body);
-    }, duration);
+        duration,
+      })
+      navigator.sendBeacon('/api/log', body)
+    }, duration)
 
     return function cleanup() {
-      clearInterval(id);
-    };
-  }, [roomId]);
+      if (!isProduction) return
+
+      clearInterval(id)
+    }
+  }, [roomId])
 }
 
 export function useLogRoomDuration({ roomId }: any) {
   const send = useCallback(
     ({ startedAt, event }) => {
-      if (!navigator.sendBeacon) return;
+      if (!navigator.sendBeacon) return
 
-      const endedAt = Date.now();
-      const duration = endedAt - startedAt;
+      const endedAt = Date.now()
+      const duration = endedAt - startedAt
       const body = formatBody({
-        type: "room_duration",
+        type: 'room_duration',
         roomId,
         startedAt,
         endedAt,
         duration,
-        event
-      });
-      navigator.sendBeacon("/api/log", body);
+        event,
+      })
+      navigator.sendBeacon('/api/log', body)
     },
     [roomId]
-  );
+  )
 
   useEffect(() => {
-    const startedAt = Date.now();
-    const event = "unmount";
+    if (!isProduction) return
+
+    const startedAt = Date.now()
+    const event = 'unmount'
 
     return function cleanup() {
-      send({ startedAt, event });
-    };
-  }, [send]);
+      if (!isProduction) return
+
+      send({ startedAt, event })
+    }
+  }, [send])
 
   useEffect(() => {
-    const startedAt = Date.now();
-    const event = "unload";
+    if (!isProduction) return
+
+    const startedAt = Date.now()
+    const event = 'unload'
 
     function handleUnload() {
-      send({ startedAt, event });
+      send({ startedAt, event })
     }
-    window.addEventListener("unload", handleUnload);
+    window.addEventListener('unload', handleUnload)
 
     return function cleanup() {
-      window.removeEventListener("unload", handleUnload);
-    };
-  }, [send]);
+      if (!isProduction) return
+
+      window.removeEventListener('unload', handleUnload)
+    }
+  }, [send])
 }
