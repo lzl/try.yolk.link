f5d5a66b613b62f8848b6f00d3fbab4b536ddd71
lizunlong <lizunlong@gmail.com>
2019-12-24 11:58:48 +0800

    rolling if button's state is loading

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import classNames from 'classnames'
-import Loader from './Loader'
 
 const Button = (props: any) => {
   return (
     <button
       className={classNames(props.className, {
         'opacity-50 cursor-not-allowed': props.disabled,
+        rolling: props.loading,
       })}
       disabled={props.disabled}
       onClick={props.onClick}
     >
       {props.children}
-      {props.loading && (
-        <div className="ml-1">
-          <Loader />
-        </div>
-      )}
     </button>
   )
 }
