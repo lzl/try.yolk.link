80314eee3b7adeb8517274e81a6adc26592f73ff
lizunlong <lizunlong@gmail.com>
2019-12-26 16:45:27 +0800

    disabled VConsole

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Router } from '@reach/router'
 import Home from './page/Home'
 import Room from './page/Room'
 import './App.css'
-import VConsole from 'vconsole'
+// import VConsole from 'vconsole'
 import packageJson from '../package.json'
 
 console.log('version:', `v${packageJson.version}`)
 
-new VConsole()
+// new VConsole()
 
 const App: React.FC = () => {
   return (
