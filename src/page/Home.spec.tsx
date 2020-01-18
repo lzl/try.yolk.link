2da530d15549d44d0a33e2f5819493db40b7b4bf
lizunlong <lizunlong@gmail.com>
2020-01-18 15:50:55 +0800

    use fetch mock

--- src/page/Home.spec.tsx
+++ src/page/Home.spec.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
 
 import Home from './Home'
+
+const fetchMock = require('fetch-mock-jest')
 
 it('Shows "Give Yolk Link a try"', () => {
   const { getByText } = render(<Home />)
 
   expect(getByText('Give Yolk Link a try')).not.toBeNull()
+})
+
+it('Shows "Get Started" button', () => {
+  const { getByText } = render(<Home />)
+  const button = getByText('Get Started')
+
+  expect(button).not.toBeNull()
+})
+
+it('Shows "Go to your Yolk Link" button after create room', async () => {
+  fetchMock.mock('/api/room-create', { roomId: 'fakeRoomId' })
+
+  const { getByText } = render(<Home />)
+  const button = getByText('Get Started')
+  fireEvent.click(button)
+
+  const title = 'Go to your Yolk Link'
+  await waitForElement(() => getByText(title))
+
+  expect(getByText(title)).not.toBeNull()
 })
 
 it('Should match Snapshot', () => {
